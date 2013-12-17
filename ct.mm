371478969f6943b6100d5e2aed7f859685bd632b
Rocky Dunlap <rocky@gatech.edu>
2013-12-17 11:43:53 -0500

    Adding FreeMind file to test

---
+++ ct.mm
@@ -0,0 +1,61 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<attribute_registry SHOW_ATTRIBUTES="hide"/>
+<node CREATED="1387291630635" ID="ID_112882699" MODIFIED="1387298142814">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Coupling Technology
+    </p>
+  </body>
+</html></richcontent>
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      What is there?
+    </p>
+  </body>
+</html>
+</richcontent>
+<hook NAME="accessories/plugins/CreationModificationPlugin_new.properties"/>
+<hook NAME="accessories/plugins/RevisionPlugin.properties"/>
+<attribute_layout NAME_WIDTH="72" VALUE_WIDTH="199"/>
+<attribute NAME="status" VALUE="good"/>
+<attribute NAME="name" VALUE="CT"/>
+<attribute NAME="date" VALUE="12-12-13"/>
+<node CREATED="1387291874838" ID="ID_423278736" MODIFIED="1387298291385" POSITION="right" TEXT="Coupling Functions">
+<font NAME="SansSerif" SIZE="12"/>
+<attribute NAME="status" VALUE="good"/>
+</node>
+<node CREATED="1387291885175" ID="ID_1124730387" MODIFIED="1387298266951" POSITION="right" TEXT="Utility Functions">
+<attribute NAME="status" VALUE="good"/>
+<node CREATED="1387291957909" ID="ID_457996419" MODIFIED="1387297229906" TEXT="has calendar">
+<attribute NAME="status" VALUE="good"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ffff00" COLOR="#000000" CREATED="1387291869870" ID="ID_973882573" MODIFIED="1387298551498" POSITION="right" TEXT="Architecture">
+<edge COLOR="#111111" WIDTH="thin"/>
+<icon BUILTIN="help"/>
+<attribute NAME="status" VALUE="good"/>
+<node BACKGROUND_COLOR="#ffff00" COLOR="#111111" CREATED="1387291893683" FOLDED="true" ID="ID_1114303253" MODIFIED="1387298142804" TEXT="has components">
+<edge COLOR="#111111" WIDTH="thin"/>
+<attribute NAME="status" VALUE="good"/>
+</node>
+<node COLOR="#111111" CREATED="1387291897478" ID="ID_159372" MODIFIED="1387298397106" TEXT="has driver">
+<edge COLOR="#111111" WIDTH="thin"/>
+<attribute NAME="status" VALUE="good"/>
+<node COLOR="#111111" CREATED="1387291907220" ID="ID_44635431" MODIFIED="1387297229904" TEXT="has customizable driver">
+<edge COLOR="#111111" WIDTH="thin"/>
+<attribute NAME="status" VALUE="good"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ffff00" CREATED="1387298128756" ID="ID_1354993563" MODIFIED="1387298132255" TEXT="test"/>
+</node>
+</node>
+</map>
