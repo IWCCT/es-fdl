c5c328f42ed76b85b7ecb83e6b39bb7d1f40a75f
Rocky Dunlap <rocky@gatech.edu>
2013-12-17 11:51:47 -0500

    Update FreeMind for test

--- ct.mm
+++ ct.mm
@@ -1,7 +1,7 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <attribute_registry SHOW_ATTRIBUTES="hide"/>
-<node CREATED="1387291630635" ID="ID_112882699" MODIFIED="1387298142814">
+<node COLOR="#000000" CREATED="1387291630635" ID="ID_112882699" LINK="../../../Desktop/arch.mm" MODIFIED="1387299043129">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -23,39 +23,68 @@
   </body>
 </html>
 </richcontent>
+<font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/CreationModificationPlugin_new.properties"/>
 <hook NAME="accessories/plugins/RevisionPlugin.properties"/>
 <attribute_layout NAME_WIDTH="72" VALUE_WIDTH="199"/>
 <attribute NAME="status" VALUE="good"/>
 <attribute NAME="name" VALUE="CT"/>
 <attribute NAME="date" VALUE="12-12-13"/>
-<node CREATED="1387291874838" ID="ID_423278736" MODIFIED="1387298291385" POSITION="right" TEXT="Coupling Functions">
-<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#0033ff" CREATED="1387291874838" ID="ID_423278736" MODIFIED="1387298863885" POSITION="right" TEXT="Coupling Functions">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
 <attribute NAME="status" VALUE="good"/>
 </node>
-<node CREATED="1387291885175" ID="ID_1124730387" MODIFIED="1387298266951" POSITION="right" TEXT="Utility Functions">
+<node COLOR="#0033ff" CREATED="1387291885175" ID="ID_1124730387" MODIFIED="1387298881725" POSITION="right" TEXT="Utility Functions">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
 <attribute NAME="status" VALUE="good"/>
-<node CREATED="1387291957909" ID="ID_457996419" MODIFIED="1387297229906" TEXT="has calendar">
+<node COLOR="#00b439" CREATED="1387291957909" ID="ID_457996419" MODIFIED="1387298863905" TEXT="has calendar">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
 <attribute NAME="status" VALUE="good"/>
 </node>
 </node>
-<node BACKGROUND_COLOR="#ffff00" COLOR="#000000" CREATED="1387291869870" ID="ID_973882573" MODIFIED="1387298551498" POSITION="right" TEXT="Architecture">
-<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#0033ff" CREATED="1387291869870" ID="ID_973882573" MODIFIED="1387298881731" POSITION="right" TEXT="Architecture">
+<edge COLOR="#111111" STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
 <icon BUILTIN="help"/>
 <attribute NAME="status" VALUE="good"/>
-<node BACKGROUND_COLOR="#ffff00" COLOR="#111111" CREATED="1387291893683" FOLDED="true" ID="ID_1114303253" MODIFIED="1387298142804" TEXT="has components">
-<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#00b439" CREATED="1387291893683" FOLDED="true" ID="ID_1114303253" MODIFIED="1387298863922" TEXT="has components">
+<edge COLOR="#111111" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
 <attribute NAME="status" VALUE="good"/>
 </node>
-<node COLOR="#111111" CREATED="1387291897478" ID="ID_159372" MODIFIED="1387298397106" TEXT="has driver">
-<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#00b439" CREATED="1387291897478" FOLDED="true" ID="ID_159372" MODIFIED="1387298870932" TEXT="has driver">
+<edge COLOR="#111111" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
 <attribute NAME="status" VALUE="good"/>
-<node COLOR="#111111" CREATED="1387291907220" ID="ID_44635431" MODIFIED="1387297229904" TEXT="has customizable driver">
+<node COLOR="#990000" CREATED="1387291907220" ID="ID_44635431" MODIFIED="1387298863927" TEXT="has customizable driver">
 <edge COLOR="#111111" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
 <attribute NAME="status" VALUE="good"/>
 </node>
 </node>
-<node BACKGROUND_COLOR="#ffff00" CREATED="1387298128756" ID="ID_1354993563" MODIFIED="1387298132255" TEXT="test"/>
+<node COLOR="#00b439" CREATED="1387298128756" ID="ID_1354993563" MODIFIED="1387298863935" TEXT="test">
+<edge COLOR="#111111" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1387298653429" ID="ID_1556789036" MODIFIED="1387298881737" POSITION="left" TEXT="Metadata">
+<edge COLOR="#111111" STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#ffff00" COLOR="#00b439" CREATED="1387298699993" ID="ID_91518500" MODIFIED="1387298863943" TEXT="Source">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ffff00" COLOR="#00b439" CREATED="1387298705235" ID="ID_1145253824" MODIFIED="1387298863946" TEXT="Something">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1387298660389" ID="ID_931791321" MODIFIED="1387298863948" POSITION="left" TEXT="Interaction Protocols">
+<edge COLOR="#111111" STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
 </node>
 </node>
 </map>
