4082a4e93aaaf62ed484e13a617bf06618eaa1a0
Rocky Dunlap <rocky@gatech.edu>
2013-12-17 12:08:22 -0500

    New mindmaps

---
+++ architecture.mm
@@ -0,0 +1,18 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1387299778316" ID="ID_1789217522" MODIFIED="1387299917774" TEXT="Architecture">
+<node CREATED="1387299813312" ID="ID_512209771" MODIFIED="1387299816445" POSITION="right" TEXT="has coupling functions"/>
+<node CREATED="1387299817965" ID="ID_442686054" MODIFIED="1387299821070" POSITION="right" TEXT="has utility functions"/>
+<node CREATED="1387299821438" ID="ID_928299174" MODIFIED="1387299825691" POSITION="right" TEXT="has graphical user interface"/>
+<node CREATED="1387299827411" ID="ID_75143796" MODIFIED="1387299829464" POSITION="right" TEXT="has components">
+<node CREATED="1387299868606" ID="ID_339170951" MODIFIED="1387299871970" TEXT="has nested components"/>
+</node>
+<node CREATED="1387299831846" ID="ID_1549301453" MODIFIED="1387299835711" POSITION="right" TEXT="control mode">
+<node CREATED="1387299852330" ID="ID_1757255913" MODIFIED="1387299853755" TEXT="is called"/>
+<node CREATED="1387299855005" ID="ID_566579441" MODIFIED="1387299856599" TEXT="is calling">
+<node CREATED="1387299859733" ID="ID_1565894528" MODIFIED="1387299864669" TEXT="has customizable driver"/>
+</node>
+</node>
+<node CREATED="1387299837015" FOLDED="true" ID="ID_36255719" MODIFIED="1387299848789" POSITION="right" TEXT="SPMD vs MPMD organization"/>
+</node>
+</map>
---
+++ es-fdl.mm
@@ -0,0 +1,32 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#cc3300" CREATED="1387299242489" ID="ID_648842869" MODIFIED="1387300035510" TEXT="Coupling Technology">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Should the top-level concept be &quot;Coupling Technology&quot; or &quot;Earth System Framework?&quot;
+    </p>
+    <ul>
+      <li>
+        the former implies the software is used for coupling (i.e., may not be helpful for standalone model)
+      </li>
+      <li>
+        the latter implies a framework, which carries technical connotations of having a predefined architecture and calls back to use code
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<hook NAME="accessories/plugins/RevisionPlugin.properties"/>
+<hook NAME="accessories/plugins/CreationModificationPlugin_new.properties"/>
+<node CREATED="1387299602889" ID="ID_1151576401" LINK="architecture.mm" MODIFIED="1387299935640" POSITION="right" TEXT="Architecture"/>
+<node CREATED="1387299605972" ID="ID_1115307351" MODIFIED="1387299612695" POSITION="right" TEXT="Coupling Functions"/>
+<node CREATED="1387299613118" ID="ID_1119378282" MODIFIED="1387299618409" POSITION="right" TEXT="Utility Functions"/>
+<node CREATED="1387299618732" ID="ID_1066621686" MODIFIED="1387299621641" POSITION="right" TEXT="Metadata"/>
+<node CREATED="1387299621975" ID="ID_1842520373" MODIFIED="1387299627089" POSITION="right" TEXT="Interaction Protocols"/>
+<node CREATED="1387299627430" ID="ID_880950894" MODIFIED="1387299631294" POSITION="right" TEXT="Implementation"/>
+</node>
+</map>
