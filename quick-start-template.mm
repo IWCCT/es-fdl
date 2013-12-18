b15911f182ab3105896b4d10cb58f4b7ec852476
Rocky Dunlap <rocky@gatech.edu>
2013-12-17 22:18:04 -0500

    Added the quick start template
    The quick start template shows conventions we can use to facilitate
    collaborating with the mindmaps.

---
+++ quick-start-template.mm
@@ -0,0 +1,64 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1387334798408" ID="ID_200211215" MODIFIED="1387335432324" TEXT="Template">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This template mindmap shows conventions we can use to collaborate on the ES-FDL mindmaps.&#160;&#160;These notes can be used both to give a description/definition of the node and also to pose questions and have discussion.
+    </p>
+  </body>
+</html>
+</richcontent>
+<hook NAME="accessories/plugins/CreationModificationPlugin_new.properties"/>
+<node COLOR="#999900" CREATED="1387334891581" ID="ID_132201574" MODIFIED="1387335464974" POSITION="right" TEXT="A subtopic with a question.">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This subtopic is colored yellow and has a question mark.&#160;&#160;It means there is an open question about this node which has been posed to the group. The question itself is typed into this notes field.&#160;&#160;This node was styled by right clicking and selecting Physical Style--&gt;Question.
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1387334956006" ID="ID_992574786" MODIFIED="1387335169385" POSITION="left" TEXT="A modified topic">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Modification timestamps can be shown by selecting Tools-&gt;Show Modification Times.&#160;&#160;Then, hover over an element to see the dates, plus any notes.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1387334962227" ID="ID_1145400882" MODIFIED="1387336288551" POSITION="left" TEXT="A node with attributes">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This node has two attributes (key-value pairs). Attributes are added by right clicking on the node and clicking &quot;Edit Attributes.&quot;&#160;&#160;The keys are values can be made up, but we could agree upon a set of attributes as the need arises.&#160;&#160;This could be a powerful feature later because filters can be created based on attributes (e.g., show all nodes where status=draft).&#160;&#160;Show the filter dialog by clicking the filter button in the toolbar.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      The attribute table itself can be shown/hidden under the View menu.
+    </p>
+  </body>
+</html>
+</richcontent>
+<attribute NAME="version" VALUE="0.1"/>
+<attribute NAME="status" VALUE="draft"/>
+</node>
+</node>
+</map>
